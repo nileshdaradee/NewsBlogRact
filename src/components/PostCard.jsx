8e55e0dc83c19ca7041d69a05393478b90fc1e50
nileshd1 <nileshdarade36@gmail.com>
2024-07-06 00:12:09 +0530

    postcard design

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-function PostCard({ $id, title, featuredImage }) {
+function PostCard({$id, title, featuredImage}) {
+    
   return (
-    <Link to={`/post/${$id}`} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-      <div className="bg-gray-100 rounded-xl p-4 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
-        <div className="w-full flex justify-center mb-4">
-          <img 
-            src={appwriteService.getFilePreview(featuredImage)} 
-            alt={title}
-            className="rounded-xl w-full h-48 object-cover" 
-          />
+    <Link to={`/post/${$id}`}>
+        <div className='flex flex-wrap w-full bg-gray-100 rounded-xl p-4 '>
+            <div className='w-full justify-center mb-4'>
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                className='rounded-xl' />
+
+            </div>
+            <h2
+            className='text-xl font-bold '
+            >{title}</h2>
         </div>
-        <h2 className="text-xl font-bold text-center">
-          {title}
-        </h2>
-      </div>
     </Link>
   )
 }
+
 
 export default PostCard
